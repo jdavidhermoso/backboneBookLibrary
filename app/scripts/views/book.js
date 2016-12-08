8619a5321987c49178c54f5e4c1179a220293188
Juan David Hermoso <juandavidhermoso@gmail.com>
2016-12-08 22:20:30 +0100

    Add and remove books capability built

--- app/scripts/views/book.js
+++ app/scripts/views/book.js
@@ -3,9 +3,15 @@ app.BookView = Backbone.View.extend({
   tagName: 'div',
   className: 'bookContainer',
   template: _.template( $( '#bookTemplate' ).html() ),
-
+  events: {
+    'click .delete': 'deleteBook'
+  },
   render: function() {
     this.$el.html( this.template( this.model.attributes ) );
     return this;
-  }
+  },
+  deleteBook: function() {
+    this.model.destroy();
+    this.remove();
+  },
 });
