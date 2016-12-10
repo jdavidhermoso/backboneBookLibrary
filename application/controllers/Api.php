b45041b29eabf899dc9bd4814e4010b97abd2f38
Juan David Hermoso <juandavidhermoso@gmail.com>
2016-12-10 12:25:42 +0100

    Removed index.php from url

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,9 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Api extends CI_Controller {
   public function index()
   {
-    $this->load->view('home');
+  }
+  public function books()
+  {
+    echo 'b';
   }
 }
